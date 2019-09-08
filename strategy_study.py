29509e9e7262410275a92e42407cd8df334ecdad
linkemhom <345852974@qq.com>
2019-09-08 13:20:43 +0800

    Modify

--- strategy_study.py
+++ strategy_study.py
@@ -14,40 +14,47 @@ class BuyAndHoldStrategy(AbstractStrategy):
     """
     def __init__(
         self, symbol, events_queue,
-        base_quantity=100
+        base_quantity=1000
     ):
         self.symbol = symbol
         self.events_queue = events_queue
         self.base_quantity = base_quantity
         self.invested = False
+        self.pre_price = 0
+        self.bar_count = 0
 
     def calculate_signals(self, event):
         if (
             event.type == EventType.BAR and
             event.symbol == self.symbol
         ):
-            if not self.invested:
+            
+            position = self.get_symbol_position(self.symbol)
+            avg_price = position["avg_price"]
+            if avg_price == 0:
+                avg_price = self.pre_price
+            if self.pre_price == 0 or (event.close_price <= avg_price * 0.9 and event.close_price >= avg_price * 0.85):
                 signal = SignalEvent(
                     self.symbol, event.timestamp, "BUY",
                     suggested_quantity=self.base_quantity
                 )
                 self.events_queue.put(signal)
-                self.invested = True
-            elif self.invested and :
+                self.pre_price = event.close_price
+            elif event.close_price >= avg_price * 1.05 or event.close_price < avg_price * 0.8:
                 signal = SignalEvent(
                     self.symbol, event.timestamp, "SELL",
                     suggested_quantity=self.base_quantity
                 )
+                self.pre_price = event.close_price
                 self.events_queue.put(signal)
-                self.invested = False
 
 
 def run(testing, symbol_list, filename):
     # Backtest information
     title = ['Buy and Hold Example on %s' % symbol_list[0]]
     initial_equity = 100000.0
-    start_date = datetime.datetime(2013, 1, 3)
-    end_date = datetime.datetime(2013, 1, 30)
+    start_date = datetime.datetime(2009, 1, 1)
+    end_date = datetime.datetime(2010, 1, 1)
 
     # Use the Buy and Hold Strategy
     events_queue = queue.Queue()
@@ -59,7 +66,7 @@ def run(testing, symbol_list, filename):
         strategy, symbol_list,
         initial_equity, start_date, end_date,
         events_queue, data_dir, output_dir, title=title,
-        #benchmark = symbol_list[1]
+        benchmark = symbol_list[1]
     )
     results = backtest.start_trading(testing=testing)
     return results
@@ -68,6 +75,6 @@ def run(testing, symbol_list, filename):
 if __name__ == "__main__":
     # Configuration data
     testing = False
-    symbol_list = ["AAPL"]
+    symbol_list = ["000001SZ_D", "000001SZ_D"]
     filename = None
     run(testing, symbol_list, filename)
